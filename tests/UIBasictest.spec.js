3834a6898f66460fca0f22094f5f3d3a09b49183
LeThinhPhat <lethinhphat7102003@gmail.com>
2026-04-27 10:26:24 +0700

    handle child window

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -28,7 +28,7 @@ test("First playwright test", async ({ browser }) => {
   console.log(await page.locator(".card-body a").nth(1).textContent());
 });
 
-test.only("Page playwright test", async ({ page }) => {
+test("Page playwright test", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   const userName = page.locator("#username");
   const password = page.locator("#password");
@@ -44,4 +44,23 @@ test.only("Page playwright test", async ({ page }) => {
   await page.locator("#terms").uncheck();
   expect(await page.locator("#terms").isChecked()).toBeFalsy();
   // await page.pause();
+});
+
+test.only("Child window test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']");
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    documentLink.click(),
+  ]);
+  const text = await newPage.locator(".red").textContent();
+  const arrayText = text.split("@");
+  const domain = arrayText[1].split(" ")[0];
+  // console.log(domain);
+  await page.locator("#username").fill(domain);
+  // await page.pause();
+  console.log(await page.locator("#username").inputValue());
 });
