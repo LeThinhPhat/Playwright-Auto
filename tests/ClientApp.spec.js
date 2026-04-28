0f86e46e86d2aec9711f311140c98965874405c0
LeThinhPhat <lethinhphat7102003@gmail.com>
2026-04-28 12:11:56 +0700

    final end to end

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,12 +1,12 @@
 const { test, expect } = require("@playwright/test");
 
 test("Client App test", async ({ page }) => {
-  const email = "anshika@gmail.com";
+  const email = "lethinhphat7102003@gmail.com";
   const productName = "ZARA COAT 3";
   const products = page.locator(".card-body");
   await page.goto("http://rahulshettyacademy.com/client");
   await page.locator("#userEmail").fill(email);
-  await page.locator("#userPassword").fill("Iamking@000");
+  await page.locator("#userPassword").fill("Phat12345");
   await page.locator("[value='Login']").click();
   await page.waitForLoadState("networkidle");
   // await page.locator(".card-body b").first().waitFor();
@@ -50,5 +50,18 @@ test("Client App test", async ({ page }) => {
     .locator(".em-spacer-1 .ng-star-inserted")
     .textContent();
   console.log(orderId);
+  await page.locator("button[routerlink*='myorders']").click();
+  await page.locator("tbody").waitFor();
+  const rows = page.locator("tbody tr");
+
+  for (let i = 0; i < (await rows.count()); ++i) {
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId)) {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy();
   await page.pause();
 });
